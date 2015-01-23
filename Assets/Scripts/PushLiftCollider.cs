0aa96117c84ffd6c36b7a72bd161cd2c33dcca1c
LeftRight92 <Michael_Bohea@msn.com>
2015-01-23 21:21:28 +0000

    Added PushLift detection

---
+++ Assets/Scripts/PushLiftCollider.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushLiftCollider : MonoBehaviour {
+
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Entering "+other.transform);
+		if(other.transform.tag == "Interactable"){
+			Debug.Log ("Setting current push lift target to "+other.transform);
+			target = other.transform;
+		}
+	
+	}
+	
+	void OnTriggerExit2D(Collider2D other){
+		Debug.Log ("Exiting" +other.transform);
+		if(other.transform.tag == "Interactable"){
+			Debug.Log ("Unsetting push lift target");
+			target = null;
+		}
+	}
+	
+}
