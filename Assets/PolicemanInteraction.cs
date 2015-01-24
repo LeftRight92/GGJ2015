343f42df2b8fca17062ce66f8b36e88681f35b17
LeftRight92 <Michael_Bohea@msn.com>
2015-01-24 21:40:48 +0000

    Policeman Animation and Prefabs

---
+++ Assets/PolicemanInteraction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PolicemanInteraction : MonoBehaviour, InteractableObject {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public bool onLift(){
+		return true;
+	}
+	
+	public bool onPush(bool rightFacing){
+	
+		return true;
+	}
+}
