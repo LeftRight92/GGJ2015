3dcd30211607a2113030bb24ed9728f6a17d0a47
disblader <domas123123@gmail.com>
2015-01-24 21:03:12 +0000

    Child idle/hurt

--- Assets/Scripts/Interaction Scripts/KidInteraction.cs
+++ Assets/Scripts/Interaction Scripts/KidInteraction.cs
@@ -5,15 +5,34 @@ public class KidInteraction : MonoBehaviour, InteractableObject {
 
 	private Animator kidAnimator;
 	private Animator catAnimator;
+	private bool isIdle; //If is not idle, then is hurt (when considering the new sound in looping
+	private bool isBusy; //Used to check if the idle sounds can be played
 	public AudioClip[] pushed, lifted, idle, satan, hurt;
+
 	// Use this for initialization
 	void Start () {
 		kidAnimator = transform.GetComponentInChildren<Animator>();
+		InvokeRepeating ("IdleSound", 0, 3);
+		isIdle = true;
 	}
-	
+
+
+	void IdleSound(){
+		if (!isBusy) {
+						if (isIdle) {
+								audio.clip = idle [Random.Range (0, idle.GetLength (0))];
+								audio.Play ();	
+						} else {
+								audio.clip = hurt [Random.Range (0, hurt.GetLength (0))];
+								audio.Play ();
+						}
+				}
+
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 	
 	public bool onPush(bool facing){
@@ -24,12 +43,16 @@ public class KidInteraction : MonoBehaviour, InteractableObject {
 
 	IEnumerator kidLiftedEvent()
 	{
+		isBusy = true;
+		isIdle = false;
+
 		transform.Translate(new Vector3(0, 3.5f, 0));
 		PlayerController player = GameController.Get ("Player").GetComponent<PlayerController>();
 		catAnimator = GameController.Get ("Cat").GetComponent<Animator> ();
 		player.canControl = false;
 		kidAnimator.SetTrigger ("Reach");
 		audio.clip = lifted[Random.Range (0,lifted.GetLength (0))];
+		audio.loop = false;
 		audio.Play ();
 		yield return new WaitForSeconds(1);
 		catAnimator.SetBool ("isScratching", true);
@@ -42,8 +65,9 @@ public class KidInteraction : MonoBehaviour, InteractableObject {
 		transform.Translate(new Vector3(0, -3.5f, 0));
 		player.canControl = true;
 		transform.tag = "Untagged";
-		Destroy (this);
-
+//		Destroy (this);
+		
+		isBusy = false;
 	}
 
 
