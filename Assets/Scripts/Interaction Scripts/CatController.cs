e19001963e4c91179938ad4dfcbf34685de85339
disblader <domas123123@gmail.com>
2015-01-24 21:10:26 +0000

    Cat idle

--- Assets/Scripts/Interaction Scripts/CatController.cs
+++ Assets/Scripts/Interaction Scripts/CatController.cs
@@ -3,9 +3,24 @@ using System.Collections;
 
 public class CatController : MonoBehaviour {
 
+	public AudioClip[] idle;
+	private bool isBusy;
+
 	// Use this for initialization
 	void Start () {
 		GameController.Register ("Cat", transform);
+		InvokeRepeating ("IdleSound", 0, 5);
+		isBusy = false;
+	}
+	
+	
+	void IdleSound(){
+
+		if (!isBusy) {
+			audio.clip = idle [Random.Range (0, idle.GetLength (0))];
+			audio.Play ();	
+		}
+		
 	}
 	
 	// Update is called once per frame
