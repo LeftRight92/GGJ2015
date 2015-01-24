e0ca217d94a751d0a19884a2416d463b7ffa30f8
LeftRight92 <Michael_Bohea@msn.com>
2015-01-24 23:40:55 +0000

    I dun did a tree

---
+++ Assets/Scripts/Interaction Scripts/TreeInteraction.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeInteraction : MonoBehaviour, InteractableObject {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public bool onLift(){
+		return false;
+	}
+	
+	public bool onPush(bool right){
+		return true;
+	}
+	
+}
