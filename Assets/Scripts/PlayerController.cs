43d6497b348b717528f05648abaa341ca37b93f2
LeftRight92 <Michael_Bohea@msn.com>
2015-01-24 15:20:41 +0000

    Push Pull Anims
    Warning: Errors

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,42 +12,35 @@ public class PlayerController : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
+	void Update () {		
 		// On Left Mouse click
 		if(Input.GetButtonDown ("Push")){
-			//Debug.Log ("Pushing!");
+			animator.SetTrigger("Push");
+			Debug.Log ("Pushing!");
 
 			// Get the target object
 			Transform target = transform.GetComponentInChildren<PushLiftCollider>().getTarget();
-
-			// Get the interactableObject script for the target object
-			InteractableObject targetScript = (InteractableObject) target.GetComponent(typeof(InteractableObject));
-
-			//Debug.Log (target);
-			//Debug.Log (targetScript != null);
-
-			// Execute on push command
-			targetScript.onPush();
-
+			if(target != null){
+				// Get the interactableObject script for the target object
+				InteractableObject targetScript = (InteractableObject) target.GetComponent(typeof(InteractableObject));
+				// Execute on push command
+				targetScript.onPush();
+			}
 		}	
 		// On Right Mouse click
 		if(Input.GetButtonDown ("Lift")){
-			//Debug.Log ("Do you maybe lift?");
+			animator.SetTrigger ("Lift");
+			Debug.Log ("Do you maybe lift?");
 
 			// Get the target object
 			Transform target = transform.GetComponentInChildren<PushLiftCollider>().getTarget();
-
-			// Get the interactableObject script for the target object
-			InteractableObject targetScript = (InteractableObject) target.GetComponent(typeof(InteractableObject));
-
-			//Debug.Log (target);
-			//Debug.Log (targetScript != null);
-
-			// Execute on lift command
-			targetScript.onLift();
+			if(target != null){
+				// Get the interactableObject script for the target object
+				InteractableObject targetScript = (InteractableObject) target.GetComponent(typeof(InteractableObject));
+				// Execute on lift command
+				targetScript.onLift();
+			}			
 		}
-
 		// On left right movement
 		if(Input.GetAxis ("Horizontal") > 0){
 			transform.Translate (new Vector3(moveSpeed * Time.deltaTime,0,0));
