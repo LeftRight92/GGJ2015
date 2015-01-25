ea76ba590a415606c26722533f8bcabd41a4e14d
disblader <domas123123@gmail.com>
2015-01-25 13:05:23 +0000

    Fireman script pushed

--- Assets/Scripts/Interaction Scripts/FiremanInteraction.cs
+++ Assets/Scripts/Interaction Scripts/FiremanInteraction.cs
@@ -5,6 +5,7 @@ public class FiremanInteraction : MonoBehaviour, InteractableObject {
 
 	private Animator firemanAnimator;
 	private bool isBusy;
+	private GameObject waterObject;
 
 	public AudioClip idle;
 	public GameObject hoze;
@@ -40,8 +41,7 @@ public class FiremanInteraction : MonoBehaviour, InteractableObject {
 		}
 
 		//spawn water
-		// local
-		GameObject waterObject = Instantiate (water, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
+		waterObject = Instantiate (water, new Vector3 (0, 0, 0), Quaternion.identity) as GameObject;
 		waterObject.transform.parent = transform;
 		waterObject.transform.localPosition = new Vector3 (-4.11f, -1.5f, 0);
 		waterObject.GetComponentInChildren<SpriteRenderer> ().sortingOrder = 2;
@@ -66,23 +66,50 @@ public class FiremanInteraction : MonoBehaviour, InteractableObject {
 	}
 	
 	public bool onPush(bool rightFacing){
-		
+
 		StartCoroutine ("BoomHeadshot");
 		return true;
 	}
 	
 	IEnumerator BoomHeadshot(){
 
+		GameController.Get ("Fireman").tag = "Untagged";
+		GameController.Get("Player").GetComponentInChildren<PushLiftCollider>().becomeUninteractable(GameController.Get ("Fireman"));
+		
 		//Rotate clockwise 90 degrees
-		while(transform.rotation.eulerAngles.z > 270){			
+		while(Mathf.Abs(transform.rotation.eulerAngles.z - 270) > 3){				
 			transform.Rotate(Vector3.back * Time.deltaTime* 120); //it's good enough
 			yield return null;			
 		}
 
+		// Play fireman crazy sounds
+		// stretch the water 
+
+		Transform water_sprite = waterObject.transform.GetChild (0);
+		SpriteRenderer renderer = waterObject.GetComponentInChildren<SpriteRenderer>();
+		Debug.Log ("spriteRenderer " + renderer.ToString ());
+
+//		Debug.Log ("1");		
+		while(water_sprite.transform.localScale.x < 4){				
+//			Debug.Log ("Current scale: " + water_sprite.transform.localScale.ToString() );		
+//			Debug.Log ("New scale: " + Vector3.Lerp(water.transform.localScale, new Vector3(2,1,1), Time.deltaTime * 10).ToString());  
+
+			water_sprite.transform.localScale = new Vector3(water_sprite.transform.localScale.x + 0.1f, 1, 1);
+			//			water_sprite.transform.localScale = Vector3.MoveTowards(water.transform.localScale, new Vector3(2,1,1), Time.deltaTime);
+
+			yield return null;			
+		}
+		Debug.Log ("3");		
+		
+
+		// Crash the copter
+		// Reduce the hoze
+		// Change them to background layer?
+		
 	}
 	
 	public bool onLift(){
-
+			
 		StartCoroutine ("FallOverAndDie");
 		return true;
 	}
