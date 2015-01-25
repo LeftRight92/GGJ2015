f13bff788daf4bacb827a7fcb5dbe1a30b3470e1
Yazidi <mariusdmts@gmail.com>
2015-01-25 16:09:09 +0000

    Cat things

--- Assets/Scripts/Interaction Scripts/GhostCatInteraction.cs
+++ Assets/Scripts/Interaction Scripts/GhostCatInteraction.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class GhostCatInteraction : MonoBehaviour, InteractableObject {
 
-	public AudioClip ghostSpawn, ghostAttack;
+	public AudioClip[] idle;
+	public Sprite possessed;
 	// Use this for initialization
 	void Start () {
 		GameController.Register ("GhostCat",transform);
@@ -15,15 +16,71 @@ public class GhostCatInteraction : MonoBehaviour, InteractableObject {
 	}
 	
 	public bool onPush(bool facingRight){
-	
-		return true;
+		StartCoroutine ("possessedEvent");
+			
+			return true;
 	}
-	
+
 	public bool onLift(){
-	
+		transform.parent = GameController.Get ("Player");
+		GameController.Get ("Player").GetComponent<PlayerController> ().canControl = false;
+		GameController.Get ("Player").GetComponentInChildren<Animator> ().SetBool ("Lift",true);
+		transform.localPosition = new Vector3 (0, 2f, 0);
+		transform.GetComponentInChildren<Animator> ().SetBool ("Scratch", false);
+		idle = GameController.Get ("Cat").GetComponent<CatController> ().idle;		
+		
+		InvokeRepeating ("IdleSound", 0, 4);
+		StartCoroutine ("goingToHeaven");
 		return true;
 	}
 
+	IEnumerator goingToHeaven(){
+		while (GameController.Get ("Player").transform.position.y < 5) {
+			GameController.Get ("Player").Translate(new Vector3(0, 2*Time.deltaTime, 0));
+			yield return null;
+		}
 
+		Application.LoadLevel ("heaven");
+	}
+
+	IEnumerator possessedEvent(){
+		//Chase PLayer
+		while ((transform.position.x - GameController.Get ("Player").transform.position.x) > 0.1f || (transform.position.x - GameController.Get ("Player").transform.position.x) < -0.1f){
+			transform.Translate(new Vector3(-(transform.position.x - GameController.Get ("Player").transform.position.x) * Time.deltaTime,0,0));
+			                    yield return null;
+			                    }
+
+			//Remove Control from Player
+			GameController.Get ("Player").GetComponent<PlayerController> ().canControl = false;
+			GameController.Get ("Player").GetComponentInChildren<Animator> ().enabled = false;
+
+			//Change player to possessed
+			GameController.Get ("Player").GetComponentInChildren<SpriteRenderer> ().sprite = possessed;
+
+			//Mute and remove ghost cat
+			transform.GetComponent<AudioSource>().clip = null;
+			transform.GetComponentInChildren<SpriteRenderer> ().enabled = false;
+		yield return null;
+
+		idle = GameController.Get ("Cat").GetComponent<CatController> ().idle;		
+
+		InvokeRepeating ("IdleSound", 0, 4);
+
+		GameController.Get ("Player").GetComponentInChildren<Rigidbody2D> ().gravityScale = 0;
+		while (GameController.Get ("Player").transform.position.x > -6 && GameController.Get ("Player").transform.position.y < 5) {
+			if 	(GameController.Get ("Player").position.x > -10)
+				GameController.Get ("Player").Translate(new Vector3(-2*Time.deltaTime, 0, 0));
+			if 	(GameController.Get ("Player").position.y < 5)
+				GameController.Get ("Player").Translate(new Vector3(0, 2*Time.deltaTime, 0));
+			yield return null;
+		}
+
+		Application.LoadLevel ("game_over");
+		}
+
+	void IdleSound(){
+			audio.clip = idle [Random.Range (0, idle.GetLength (0))];
+			audio.Play ();
+	}
 
 }
