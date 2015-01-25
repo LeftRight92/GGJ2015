f2142ded6f25a04100c77ef3ede81fe129a127c0
Yazidi <mariusdmts@gmail.com>
2015-01-25 16:40:46 +0000

    True End

--- Assets/Scripts/Interaction Scripts/GodInteraction.cs
+++ Assets/Scripts/Interaction Scripts/GodInteraction.cs
@@ -5,9 +5,14 @@ public class GodInteraction : MonoBehaviour, InteractableObject {
 
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine ("youDed");
 	}
-	
+
+	IEnumerator youDed() {
+		yield return new WaitForSeconds (2);
+		Application.LoadLevel ("game_over");
+		}
+
 	// Update is called once per frame
 	void Update () {
 	
